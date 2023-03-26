68a01b59c0ad2337071714c2f652a1575d149861
Buffik <Som16@mail.ru>
2023-03-26 15:14:13 +0300

    feat: add info

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './styles/scss/style.scss';
+
+let viewport = document.querySelector('.slider__wrapper').offsetWidth;
+let btnNext = document.getElementById('next');
+let btnPrev = document.getElementById('prev');
+let slider = document.querySelector('.slider__view');
+let viewSliders = document.querySelectorAll('.slider-navigation__item');
+let viewSlide = 0;
+
+viewSliders[0].style.backgroundColor = '#0074fd';
+
+btnNext.addEventListener('click', function () {
+  viewSliders[viewSlide].style.backgroundColor = '#7e7e7e';
+  if (viewSlide < 4) {
+    viewSlide++;
+  } else {
+    viewSlide = 0;
+  }
+  viewSliders[viewSlide].style.backgroundColor = '#0074fd';
+  slider.style.left = -viewSlide * viewport + 'px';
+});
+
+btnPrev.addEventListener('click', function () {
+  viewSliders[viewSlide].style.backgroundColor = '#7e7e7e';
+  if (viewSlide > 0) {
+    viewSlide--;
+  } else {
+    viewSlide = 4;
+  }
+  viewSliders[viewSlide].style.backgroundColor = '#0074fd';
+  slider.style.left = -viewSlide * viewport + 'px';
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,49 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const isProduction = process.env.NODE_ENV == 'production';
 
-const isProduction = process.env.NODE_ENV == "production";
-
-const stylesHandler = "style-loader";
+const stylesHandler = 'style-loader';
 
 const config = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    assetModuleFilename: 'src/assets/img/[name].[ext]',
   },
   devServer: {
     open: true,
-    host: "localhost",
+    host: 'localhost',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: 'index.html',
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
+        use: [stylesHandler, 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = "production";
+    config.mode = 'production';
   } else {
-    config.mode = "development";
+    config.mode = 'development';
   }
   return config;
 };
